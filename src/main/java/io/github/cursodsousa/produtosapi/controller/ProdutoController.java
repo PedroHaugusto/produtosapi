3575ed934ef4c299d6296ba06314d342ff6aad35
Pedro Henrique <pedrohaugusto12@gmail.com>
2025-01-06 14:48:52 -0300

    :construction: feat: Obtendo produto por ID

--- src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -3,11 +3,9 @@ package io.github.cursodsousa.produtosapi.controller;
 import io.github.cursodsousa.produtosapi.model.Produto;
 import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,10 +17,12 @@ public class ProdutoController {
 
     @PostMapping
     public void salvar(@RequestBody Produto produto){
-
-        /*var id = UUID.randomUUID().toString();
-        produto.setId(id);*/
-
         produtoRepository.save(produto);
     }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") UUID id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
 }
